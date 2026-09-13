d04a31df111fb1e6c0f76d5f8f9759dc3caf332a
Beau2176 <bjkoehler5@gmail.com>
2026-09-12 19:43:30 -0600

    Load advertising analytics integration

--- app/start.js
+++ app/start.js
@@ -29,6 +29,9 @@ const {
   integrateMemoryServerSource
 } = require("./memory/server-integration");
 const {
+  integrateAdvertisingAnalyticsServerSource
+} = require("./advertising/server-integration");
+const {
   registerBuiltInAgeVerificationProviders
 } = require("./age/providers/register");
 
@@ -48,6 +51,7 @@ function compileIntegratedServer({
   integratedSource = integrateScheduledTasksServerSource(integratedSource);
   integratedSource = integrateAgentServerSource(integratedSource);
   integratedSource = integrateMemoryServerSource(integratedSource);
+  integratedSource = integrateAdvertisingAnalyticsServerSource(integratedSource);
 
   const runtimeModule = new Module(serverPath, parentModule);
   runtimeModule.filename = serverPath;
