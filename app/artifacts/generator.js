43ecceebebf60659c793925741b1299ae8309ab7
boyd j koehler <bjkoehler5@gmail.com>
2026-09-18 15:00:58 -0600

    Wave 14: strip invalid XML control characters

--- app/artifacts/generator.js
+++ app/artifacts/generator.js
@@ -104,6 +104,7 @@ function createZip(entries) {
 
 function xmlEscape(value) {
   return String(value ?? "")
+    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F]/g, "")
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
